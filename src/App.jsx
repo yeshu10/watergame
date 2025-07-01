21141401a57e9332fbfaf9b9376b37738f1394d0
yeshu10 <yeshvigupta10@gmail.com>
2025-07-02 00:48:11 +0530

    Save local changes before rebase

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Water from './components/water'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Water/>
+     
+    </>
+     )
+}
+
+export default App
---
+++ src/components/water.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function water() {
+  return (
+    <>
+    <div className='bg-blue-400 height:400px width:200px'>HI</div>
+    </>
+  )
+}
+
+export default water
